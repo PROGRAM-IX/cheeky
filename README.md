0d185c8a0144ea5c2f8f5849342278b283e581c5
svexican <tetopo@users.noreply.github.com>
2014-08-26 17:57:58 -0500

    README: examples on setting coordinates

--- README.md
+++ README.md
@@ -1,4 +1,82 @@
-cheeky
-======
+# cheeky
 
 A cheeky window switcher for Awesome WM
+
+## Description
+
+It popus up a menu with all your clients. As you type,
+case-insensitively filters the clients that match with the
+name or class.
+
+It's cheeky. If no clients are matched it resets to the complete list
+of clients... and it tells you about it.
+
+It was inspired by the oh-so-great:
+
+- https://github.com/seanpringle/xowl
+- https://github.com/seanpringle/simpleswitcher
+
+## Installation
+
+
+Clone this repository into your Awesome configuration directory:
+
+```
+  git clone https://github.com/svexican/cheeky ~/.config/awesome
+```
+
+## Usage
+
+Include the module on your `rc.lua` file:
+
+```lua
+  require("cheeky")
+```
+
+and assign a shortcut in your `root.keys` table. For example:
+
+```lua
+  modkey = "Mod4"
+
+  root.keys(awful.util.table.join(
+    -- lots of your shortcuts
+
+      awful.key({ modkey }, "/", function() cheeky.util.switcher() end),
+
+    -- lots of more shortcuts
+  ))
+```
+
+#### Setting the coordinates
+
+You can set the coordinates at which you want the menu to appear like so
+
+```lua
+awful.key({ modkey }, "/", function()
+  cheeky.util.switcher({ coords = { x = 200, y = 200 } })
+end),
+```
+
+or if you want relative coordinates (per active screen) you can do
+
+```lua
+awful.key({ modkey }, "/", function()
+  local offset = screen[client.focus.screen].workarea
+
+  cheeky.util.switcher({ coords = { x = offset.x + 200,
+                                    y = offset.y + 200 } })
+end),
+```
+
+Type away!
+
+## TODO
+
+- Reduce the flickering (it's not too bad)
+- Allow the user to
+  * set a static screen and coordinates for the menu
+  * disable the cheeky reset
+  * disable the cheeky notification
+- ~~Fuzzy matching?~~ (won't do. it's impractical, too many results are generated)
+- ~~Select entry by number~~
+- ~~Auto select when single match~~ (won't do. it creates a 'human' bug)
---
+++ util.lua
@@ -0,0 +1,124 @@
+local awful   = require("awful")
+local wibox   = require("wibox")
+local naughty = require("naughty")
+
+local string = string
+local client = client
+local pairs  = pairs
+local table  = table
+
+module("cheeky.util")
+
+local matcher_str = nil
+local cmenu       = nil
+
+local options = nil
+
+local function nocase(s)
+  s = string.gsub(s, "%a", function (c)
+                    return string.format("[%s%s]",
+                                         string.lower(c),
+                                         string.upper(c)) end)
+  return s
+end
+
+function match_clients(s)
+  local cls = {}
+
+  for i, c in pairs(client.get()) do
+    local nocase_s = nocase(s)
+
+    if awful.rules.match(c, { name = nocase_s })
+      or awful.rules.match(c, { class = nocase_s })
+    then
+      table.insert(cls, { c.name, function()
+                            client.focus = c
+                            c:raise() end,
+                          c.icon }) end
+  end
+
+  return cls
+end
+
+function cmenureset()
+  if cmenu then cmenu:hide() end
+
+  local clist = match_clients(matcher_str)
+
+  if #clist == 0 then
+    naughty.notify({ text = "No matches. Reseting.", timeout = 1 })
+    matcher_str = ""
+    clist = match_clients(matcher_str)
+  end
+
+  cmenu = awful.menu(clist)
+  cmenu:item_enter(1)
+  cmenu:show(options)
+
+  awful.keygrabber.run(grabber)
+end
+
+function grabber(mod, key, event)
+  local sel = cmenu.sel or 0
+
+  if event == "release" then return end
+
+  if key == 'Down' then
+    local sel_new = (sel + 1) > #cmenu.items and 1 or (sel + 1)
+    cmenu:item_enter(sel_new)
+
+  elseif key == 'Up' then
+    local sel_new = (sel - 1) < 1 and #cmenu.items or (sel - 1)
+    cmenu:item_enter(sel_new)
+
+  elseif key == '1' or
+    key == '2' or
+    key == '3' or
+    key == '4' or
+    key == '5' or
+    key == '6' or
+    key == '7' or
+    key == '8' or
+    key == '9'
+  then
+    cmenu:exec(key + 0, { exec = true })
+    awful.keygrabber.stop(grabber)
+
+  elseif key == '0' then
+    cmenu:exec(10, { exec = true })
+    awful.keygrabber.stop(grabber)
+
+  elseif sel > 0 and key == 'Return' then
+    cmenu:exec(sel, { exec = true })
+    awful.keygrabber.stop(grabber)
+
+  elseif key == 'ISO_Level3_Shift'
+    or key == 'Shift_L'
+    or key == 'Shift_R'
+    or key == 'Control_L'
+    or key == 'Control_R'
+    or key == 'Alt_L'
+    or key == 'Super_L'
+    or key == 'Super_R' then return
+
+  elseif key == 'Escape' then
+    awful.keygrabber.stop(grabber)
+    cmenu:hide()
+
+  elseif key == "BackSpace" then
+    matcher_str = ""
+    cmenureset()
+
+  else
+    matcher_str = matcher_str .. key
+    cmenureset()
+  end
+end
+
+function switcher(opts)
+  options = opts
+  awful.keygrabber.stop(grabber)
+  matcher_str = ""
+  cmenureset()
+  awful.keygrabber.run(grabber)
+end
